5e8878badee2068e0ff7428da0f1ca6fbd2e05a7
icarium2 <icarium414@gmail.com>
2021-10-26 17:49:05 +0200

    Setup 3D space & cube testing
    Co-authored-by: felixgren <33127919+felixgren@users.noreply.github.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,30 @@ import { Canvas } from '@react-three/fiber';
 import Box from './Box';
 import styled from 'styled-components';
 import 'styled-components/macro';
+import { OrbitControls, Plane } from '@react-three/drei';
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: blue;
+  background-color: #f1f0f0;
 `;
 
 const App: React.FC = () => {
   return (
     <div id="App">
       <Container>
-        <Canvas>
-          <Box position={[0, 0, 0]} />
+        <Canvas camera={{ position: [20, 20, 20] }}>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 15, 10]} angle={0.3} />
+          <Box position={[1, 1, 1]} scale={[5, 1, 1]} />
+          <Plane
+            position={[0, 0, 0]}
+            rotation-x={Math.PI / -2}
+            args={[100, 100, 4, 4]}
+          >
+            <meshPhongMaterial attach="material" />
+          </Plane>
         </Canvas>
       </Container>
     </div>
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -3,20 +3,21 @@ import { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 export default function Box(props: JSX.IntrinsicElements['mesh']) {
-    const ref = useRef<THREE.Mesh>(null!)
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-    return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={active ? 1.5 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-  }
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1.5, 1.5, 1.5]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}

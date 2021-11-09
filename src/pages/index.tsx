2a7a9715fd80179e8871175d0f8e1bfd376c66b5
felixgren <33127919+felixgren@users.noreply.github.com>
2021-11-09 22:28:37 +0100

    Lock OrbitControls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,16 @@ const Game: React.FC = () => {
   return (
     <div className={styles.container}>
       <Canvas camera={{ position: [0, 60, 120], fov: 50 }}>
-        <OrbitControls />
+        <OrbitControls
+          maxPolarAngle={1.1}
+          minPolarAngle={0.8}
+          minDistance={50}
+          maxDistance={134.16}
+          maxAzimuthAngle={Math.PI / 2}
+          minAzimuthAngle={-Math.PI / 2}
+          rotateSpeed={0.3}
+          enablePan={false}
+        />
         <Sky sunPosition={[160, 10, -200]} distance={700} />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />

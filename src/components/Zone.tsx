50a282d8c89722c860216908348e8d05703fa03e
felixgren <33127919+felixgren@users.noreply.github.com>
2021-11-11 15:23:17 +0100

    Fix trigger

--- src/components/Zone.tsx
+++ src/components/Zone.tsx
@@ -3,7 +3,11 @@ import { useCylinder } from '@react-three/cannon';
 import Trigger from '@/components/hooks/trigger';
 
 export default function Zone() {
-  const [ref] = useCylinder(() => ({ args: [10, 10, 10], type: 'Static' }));
+  const [ref] = useCylinder(() => ({
+    isTrigger: true,
+    args: [10, 10, 10],
+    type: 'Static',
+  }));
   return (
     <group>
       <mesh position={[-20, 0, -20]} rotation={[-Math.PI / 2, 0, 0]} ref={ref}>

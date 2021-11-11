39712915bb358588c062192666a044e98a5a2fb8
felixgren <33127919+felixgren@users.noreply.github.com>
2021-11-11 22:15:15 +0100

    Merge functions & clean

--- src/components/Plane.tsx
+++ src/components/Plane.tsx
@@ -1,41 +1,18 @@
 import { usePlane, useBox, PlaneProps, BoxProps } from '@react-three/cannon';
 
-// export function Plane() {
-//   const [ref] = useBox(() => ({
-//     rotation: [-Math.PI / 2, 0, 0],
-//     args: [5, 5, 5],
-//     position: [0, 0, 0],
-//   }));
-//   return (
-//     <group>
-//       <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-//         <planeBufferGeometry attach="geometry" args={[100, 100]} />
-//         <meshLambertMaterial attach="material" color="lightblue" />
-//       </mesh>
-//       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -50.1, 0]}>
-//         <boxBufferGeometry attach="geometry" args={[100, 100, 100]} />
-//         <meshPhongMaterial attach="material" color="hotpink" />
-//       </mesh>
-//     </group>
-//   );
-// }
-
-function NewWorld({ args, position }: BoxProps) {
-  const [ref] = useBox(() => ({ args, position }));
+function CubeWorld({ args, position, rotation }: BoxProps) {
+  const [ref] = useBox(() => ({ type: 'Static', args, position }));
   return (
-    <mesh {...{ position, ref }}>
-      <boxBufferGeometry attach="geometry" args={args} />
-      <meshPhongMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
-
-function GroundPlane({ args, position, rotation }: BoxProps) {
-  return (
-    <mesh {...{ position, rotation }}>
-      <planeGeometry attach="geometry" args={args} />
-      <meshPhongMaterial attach="material" color="hotpink" />
-    </mesh>
+    <group>
+      <mesh {...{ position, ref }}>
+        <boxBufferGeometry attach="geometry" args={args} />
+        <meshPhongMaterial attach="material" color="lightblue" />
+      </mesh>
+      <mesh {...{ rotation }} position={[0, 0.05, 0]}>
+        <planeGeometry attach="geometry" args={[100, 100]} />
+        <meshPhongMaterial attach="material" color="hotpink" />
+      </mesh>
+    </group>
   );
 }
 
@@ -57,10 +34,9 @@ function FallPlane({ onCollide }: PlaneProps) {
 export default function Map() {
   return (
     <group>
-      <NewWorld args={[100, 100, 100]} position={[0, -50, 0]} />
-      <GroundPlane
+      <CubeWorld
         args={[100, 100, 100]}
-        position={[0, 0.05, 0]}
+        position={[0, -50, 0]}
         rotation={[-Math.PI / 2, 0, 0]}
       />
       <FallPlane />

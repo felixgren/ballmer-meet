b0d0368a96e0040397eb817f19aa3103fe6d3e0a
felixgren <33127919+felixgren@users.noreply.github.com>
2021-11-11 21:04:11 +0100

    Add enter/leave zone logic & zone names

--- src/components/TriggerZones.tsx
+++ src/components/TriggerZones.tsx
@@ -1,23 +1,40 @@
 import { useCylinder, CylinderProps } from '@react-three/cannon';
 import { useState, useEffect } from 'react';
+import { Color } from 'three';
 
 interface ZoneProps extends CylinderProps {
   color: string;
+  name: string;
 }
 
+const startColor = new Color();
+
 export default function Zone() {
+  // function setZone(e: any) {
+  //   e.type == 'collideBegin'
+  //     ? console.log(e)
+  //     : e.type == 'collideEnd'
+  //     ? console.log('leave zone')
+  //     : console.log(`Error! unexpected event: ${e.type}`);
+  // }
+
   function setZone(e: any) {
-    e.type == 'collideBegin'
-      ? console.log('enter zone')
-      : e.type == 'collideEnd'
-      ? console.log('leave zone')
-      : console.log(`Error! unexpected event: ${e.type}`);
+    if (e.type == 'collideBegin') {
+      console.log(`You enter ${e.target.name}`);
+      startColor.copy(e.body.material.color);
+      e.body.material.color.copy(e.target.material.color);
+    }
+    if (e.type == 'collideEnd') {
+      console.log(`You leave ${e.target.name}`);
+      e.body.material.color.copy(startColor);
+    }
   }
 
   function ZoneTrigger({
     position,
     args,
     color,
+    name,
     onCollide,
     onCollideBegin,
     onCollideEnd,
@@ -31,7 +48,7 @@ export default function Zone() {
       onCollideEnd,
     }));
     return (
-      <mesh {...{ position, ref }}>
+      <mesh {...{ position, ref }} name={name}>
         <cylinderGeometry args={args} />
         <meshStandardMaterial wireframe color={color} />
       </mesh>
@@ -41,24 +58,23 @@ export default function Zone() {
   return (
     <group>
       <ZoneTrigger
-        position={[20, 0, 20]}
+        position={[20, 5, 10]}
         args={[10, 10, 10]}
         color={'red'}
+        name={'red room is great'}
         onCollideBegin={(e) => {
-          console.log('begin collision', e);
+          setZone(e);
         }}
         onCollideEnd={(e) => {
-          console.log('end collision', e);
-        }}
-        onCollide={(e) => {
-          console.log('Collision event on red', e);
+          setZone(e);
         }}
       />
 
       <ZoneTrigger
-        position={[-10, 0, -10]}
+        position={[-20, 5, 10]}
         args={[10, 10, 10]}
         color={'blue'}
+        name={'the blue room'}
         onCollideBegin={(e) => {
           setZone(e);
         }}

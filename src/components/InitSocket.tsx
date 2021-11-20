9fc0e2f8c57f9adc58d248f91a46d9de95741e60
felixgren <33127919+felixgren@users.noreply.github.com>
2021-11-20 23:44:32 +0100

    Adding remotePlayers WIP

--- src/components/InitSocket.tsx
+++ src/components/InitSocket.tsx
@@ -1,14 +1,16 @@
 //@ts-nocheck
 import { useEffect } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 import io from 'socket.io-client';
+import * as THREE from 'three';
+
+let player = {};
+let remotePlayers = {};
 
 export default function InitSocket(props: boxProps) {
   console.log('init sockets');
   const socket = io('http://localhost:5000');
-
-  let player = {};
-  let remotePlayers = {};
+  const { scene } = useThree();
 
   socket.on('connect', () => {
     socket.on('initPlayer', (data, playerCount, remotePlayers) => {
@@ -19,7 +21,7 @@ export default function InitSocket(props: boxProps) {
       for (let i = 0; i < playerCount; i++) {
         if (remotePlayers[i] !== player.id) {
           console.log(`${remotePlayers[i]} needs to be added to the world...`);
-          initRemotePlayer(remotePlayers[i]);
+          InitRemotePlayer(remotePlayers[i], scene);
         }
       }
     });
@@ -34,7 +36,39 @@ export default function InitSocket(props: boxProps) {
 
 function initLocalPlayer() {}
 
-function initRemotePlayer(id) {}
+function InitRemotePlayer(playerID, scene) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshNormalMaterial();
+  material.color = new THREE.Color(0x000000);
+
+  const x = Math.floor(Math.random() * 20) - 10;
+  const y = Math.floor(Math.random() * 20) - 10;
+  const z = Math.floor(Math.random() * 20) - 10;
+
+  const remotePlayer = new THREE.Mesh(geometry, material);
+  remotePlayer.position.set(x, y, z);
+
+  scene.add(remotePlayer);
+
+  remotePlayers[playerID] = {};
+  remotePlayers[playerID].mesh = remotePlayer;
+  remotePlayers[playerID].positionSync = new THREE.Vector3();
+  remotePlayers[playerID].lookDirection = new THREE.Vector3();
+
+  console.log(`${playerID} added to the scene!`);
+}
+
+export function InitRemotePlayerTest(id) {
+  const x = Math.floor(Math.random() * 20) - 10;
+  const y = Math.floor(Math.random() * 20) - 10;
+  const z = Math.floor(Math.random() * 20) - 10;
+  return (
+    <mesh position={[x, y, z]}>
+      <boxBufferGeometry attach="geometry" args={[5, 5, 5]} />
+      <meshBasicMaterial attach="material" color={0x000000} />
+    </mesh>
+  );
+}
 
 function deleteRemotePlayer(id) {}
 

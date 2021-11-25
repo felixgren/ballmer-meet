4d5492fde8658298284f2dac910794659edc0685
icarium2 <icarium414@gmail.com>
2021-11-26 00:28:17 +0100

    CUSSWORDS

--- src/components/ui-components/Chat.tsx
+++ src/components/ui-components/Chat.tsx
@@ -6,13 +6,13 @@ const Chat = () => {
       <div className={styles.outputBox}>
         {/* insert socket code to conditionally render the below block */}
         <div className={styles.chatMessageWrapper}>
-          <p className={styles.sentUserMessage}>User: </p>
+          <p className={styles.sentUserMessage}>Placeholderman: </p>
           <p className={styles.chatMessage}>crazy frog is amazing</p>
         </div>
         <div className={styles.chatMessageWrapper}>
-          <p className={styles.sentUserMessage}>User: </p>
+          <p className={styles.sentUserMessage}>Placeholdarium: </p>
           <p className={styles.chatMessage}>
-            i know bro, the new album is soooo good!! =))
+            i know bro, the new album is soooo good!! :D
           </p>
         </div>
       </div>
